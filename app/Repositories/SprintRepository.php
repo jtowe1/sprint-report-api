dc46e02490297abb513f9b6ebd7e8d1dce0c5d05
Jeremiah Towe <jtowe@foreground.co>
2022-03-11 11:31:00 -0500

    Copy repositories

---
+++ app/Repositories/SprintRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Hydrators\SprintHydrator;
+use App\Loaders\SprintLoader;
+use App\Models\Sprint;
+use App\Writers\SprintWriter;
+
+class SprintRepository
+{
+    public function __construct(
+        private SprintLoader $sprintLoader,
+        private SprintHydrator $sprintHydrator,
+        private SprintWriter $sprintWriter
+    )
+    {}
+
+    public function loadById(int $id): Sprint
+    {
+        $sprintData = $this->sprintLoader->loadById($id);
+        $sprint = $this->sprintHydrator->hydrate($sprintData);
+
+        return $sprint;
+    }
+
+    public function save(Sprint $sprint): bool
+    {
+        return $this->sprintWriter->write($sprint);
+    }
+}
