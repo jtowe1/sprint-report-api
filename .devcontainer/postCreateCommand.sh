5b96fff889efa7273c63a4c60c05266c8f0e5de2
Anthony Cook <anthy.cook@gmail.com>
2022-03-31 04:19:31 +0000

    adds openssh-client

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
-apk add --no-cache git mysql-client zsh
+apk add --no-cache git mysql-client zsh openssh-client
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 cp ./.devcontainer/.p10k.zsh ~/.p10k.zsh
