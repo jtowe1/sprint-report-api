07a3aa5d08f67f88f5f650c5e7e4faf0e57b776a
Anthony Cook <anthy.cook@gmail.com>
2022-04-14 16:28:06 +0000

    removes postCreateCommand shell script

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,5 +0,0 @@
-apk add --no-cache git mysql-client zsh openssh-client
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-cp ./.devcontainer/.p10k.zsh ~/.p10k.zsh
-cp ./.devcontainer/.zshrc ~/.zshrc
