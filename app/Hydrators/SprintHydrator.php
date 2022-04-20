c0daeba635e129425d8ff8bf4d4db9d4dc558896
Anthony Cook <anthy.cook@gmail.com>
2022-04-20 19:13:17 +0000

    adds days to sprint hydrator constructor

--- app/Hydrators/SprintHydrator.php
+++ app/Hydrators/SprintHydrator.php
@@ -16,7 +16,7 @@ class SprintHydrator
             $data['length'],
             $data['total_points'],
             $data['total_goal_points'],
-            $data['days'],
+            array_key_exists('days', $data) ? $data['days'] : null,
             new Carbon($data['created_at']),
             !empty($data['updated_at']) ? new Carbon($data['updated_at']) : null
         );
